46b849da70f96c3b43fb8bd59bf4e4d6e7e820e2
Kenan Soylu <ken.soylu@hotmail.com>
2018-12-23 00:37:38 +0300

    common lib

--- script.js
+++ script.js
@@ -1,50 +1,9 @@
-// var jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM();
-// const { document } = (new JSDOM('')).window;
-// global.document = document;
-// var $ = jQuery = require('jquery')(window);
-// const AJAX = require('./lib/Service');
-// const service = new AJAX.Service();
-
-
 $(document).ready(function() {
 
     $('.breakrow').click(function(){
       $(this).nextUntil('th.breakrow').slideToggle(200);
     });
 
-
-    Date.daysBetween = function( date1, date2 ) {
-        //Get 1 day in milliseconds
-        var one_day=1000*60*60*24;
-      
-        // Convert both dates to milliseconds
-        var date1_ms = date1.getTime();
-        var date2_ms = date2.getTime();
-      
-        // Calculate the difference in milliseconds
-        var difference_ms = date2_ms - date1_ms;
-          
-        // Convert back to days and return
-        return Math.round(difference_ms/one_day); 
-      }
-
-    var populateList = function (properties) {
-        let name = properties.name;
-        let expiration = properties.expiration || new Date();
-        let temp = properties.temp || 5;
-        let volume = properties.volume || 10;
-        let remainingday = Date.daysBetween(new Date(), expiration);
-
-        let row = "<tr>" +
-        "<td>"+name+"</td>" +
-        "<td>"+remainingday+"</td>" +
-        "<td>"+temp+"</td>" +
-        "<td>"+volume+"</td>"+"</tr>";
-
-        $("#urunler").append(row);
-    }
   let name;
 
   $("#button").click(function() {
